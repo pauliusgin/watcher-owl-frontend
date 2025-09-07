94022e7911a89d79838c3a7fd4b9a8efb68a16a5
Paulius Giniūnas <paulius.giniunas@gmail.com>
2025-09-07 06:33:33 +0300

    hide the login button

--- src/components/header/HeaderContainer/HeaderContainer.tsx
+++ src/components/header/HeaderContainer/HeaderContainer.tsx
@@ -1,21 +1,21 @@
 import "./HeaderContainer.scss";
 import { NavLink } from "react-router-dom";
-import { UserMenuContainer } from "../UserMenuContainer/UserMenuContainer";
-import { useUser } from "../../../hooks/custom.hooks";
+// import { UserMenuContainer } from "../UserMenuContainer/UserMenuContainer";
+// import { useUser } from "../../../hooks/custom.hooks";
 
 const Header = () => {
-    const { user } = useUser();
+  // const { user } = useUser();
 
-    return (
-        <header className="header">
-            <nav className="header__nav" role="navigation">
-                <ul className="header__nav__list">
-                    <li className="header__nav__list-item">
-                        <NavLink className="header__nav__link" to="/">
-                            Pagrindinis
-                        </NavLink>
-                    </li>
-                    <li className="header__nav__list-item">
+  return (
+    <header className="header">
+      <nav className="header__nav" role="navigation">
+        <ul className="header__nav__list">
+          <li className="header__nav__list-item">
+            <NavLink className="header__nav__link" to="/">
+              Pagrindinis
+            </NavLink>
+          </li>
+          {/* <li className="header__nav__list-item">
                         {(!user || user.isLoggedIn === false) && (
                             <NavLink className="header__nav__link" to="/login">
                                 Prisijungti
@@ -35,11 +35,11 @@ const Header = () => {
                         <li className="header__nav__list-item">
                             <UserMenuContainer />
                         </li>
-                    )}
-                </ul>
-            </nav>
-        </header>
-    );
+                    )} */}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export { Header };
--- src/components/main/results/ResultsContainer/ResultsContainer.tsx
+++ src/components/main/results/ResultsContainer/ResultsContainer.tsx
@@ -1,62 +1,62 @@
 import "./ResultsContainer.scss";
-import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { SortingMenu } from "../SortingMenu/SortingMenu";
 import { LoadingAnimation } from "../../../shared/LoadingAnimation/LoadingAnimation";
 import { useData, useTasks, useAuth } from "../../../../hooks/custom.hooks";
-import { TaskSaveButton } from "../../tasks/TaskSaveButton/TaskSaveButton";
+// import { TaskSaveButton } from "../../tasks/TaskSaveButton/TaskSaveButton";
 import { TaskActivationButton } from "../../tasks/TaskActivationButton/TaskActivationButton";
 import { TaskNotificationSelect } from "../../tasks/TaskNotificationSelect/TaskNotificationSelect";
 import { TaskDetails } from "../../tasks/TaskItemDetails/TaskItemDetails";
 import { useState } from "react";
 
 function ResultsContainer() {
-    const { searchQuery, data, sessionData, isLoading } = useData();
-    const { saveSuccessful, taskControlsVisibility } = useTasks();
-    const { token } = useAuth();
+  const { searchQuery, data, sessionData, isLoading } = useData();
+  const { taskControlsVisibility } = useTasks();
+  const { token } = useAuth();
 
-    const [taskDetails, setTaskDetails] = useState<TaskDetails | null>(null);
+  const [taskDetails] = useState<TaskDetails | null>(null);
 
-    const location = useLocation();
-    const sessionNameDecoded = decodeURIComponent(location.state.sessionName);
+  const location = useLocation();
+  const sessionNameDecoded = decodeURIComponent(location.state.sessionName);
 
-    const handleSaveResult = (result: TaskDetails) => {
-        setTaskDetails(result);
-    };
+  // const handleSaveResult = (result: TaskDetails) => {
+  //     setTaskDetails(result);
+  // };
 
-    if (isLoading) {
-        return <LoadingAnimation />;
-    }
+  if (isLoading) {
+    return <LoadingAnimation />;
+  }
 
-    if (data && data.length === 0) {
-        return (
-            <p className="main__results_no-results">
-                {`Pagal užklausą `}
-                <span className="main__results_query-span">
-                    {searchQuery?.join(", ")}
-                </span>
-                {` rezultatų nerasta.`}
-            </p>
-        );
-    }
-
+  if (data && data.length === 0) {
     return (
-        data &&
-        data.length > 0 && (
-            <div className="main__results_container">
-                <div className="main__results_sort-container">
-                    <SortingMenu />
-                </div>
-                <p className="main__results_item-query">
-                    {`Rodomi naujausi rezultatai (${
-                        data === sessionData ? data.length : sessionData?.length
-                    }) užklausai:`}
-                    <br />
-                    <br />
-                    <span className="main__results_query-span">
-                        {sessionNameDecoded.replaceAll("+", ", ")}
-                    </span>
-                </p>
-                <div className="main__results_save-search_container">
+      <p className="main__results_no-results">
+        {`Pagal užklausą `}
+        <span className="main__results_query-span">
+          {searchQuery?.join(", ")}
+        </span>
+        {` rezultatų nerasta.`}
+      </p>
+    );
+  }
+
+  return (
+    data &&
+    data.length > 0 && (
+      <div className="main__results_container">
+        <div className="main__results_sort-container">
+          <SortingMenu />
+        </div>
+        <p className="main__results_item-query">
+          {`Rodomi naujausi rezultatai (${
+            data === sessionData ? data.length : sessionData?.length
+          }) užklausai:`}
+          <br />
+          <br />
+          <span className="main__results_query-span">
+            {sessionNameDecoded.replaceAll("+", ", ")}
+          </span>
+        </p>
+        {/* <div className="main__results_save-search_container">
                     {token ? (
                         <>
                             <TaskSaveButton onSave={handleSaveResult} />
@@ -82,19 +82,19 @@ function ResultsContainer() {
                             </NavLink>
                         </p>
                     )}
-                </div>
-                <div className="main__results_task-controls_container">
-                    {token && taskControlsVisibility && (
-                        <>
-                            <TaskActivationButton taskId={taskDetails?._id} />
-                            <TaskNotificationSelect taskId={taskDetails?._id} />
-                        </>
-                    )}
-                </div>
-                <Outlet />
-            </div>
-        )
-    );
+                </div> */}
+        <div className="main__results_task-controls_container">
+          {token && taskControlsVisibility && (
+            <>
+              <TaskActivationButton taskId={taskDetails?._id} />
+              <TaskNotificationSelect taskId={taskDetails?._id} />
+            </>
+          )}
+        </div>
+        <Outlet />
+      </div>
+    )
+  );
 }
 
 export { ResultsContainer };
